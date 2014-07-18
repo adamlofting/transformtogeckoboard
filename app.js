3f985f73534a2a9a20abb911205eac56142228b3
Adam Lofting <contactme@adamlofting.com>
2014-07-18 15:36:14 +0100

    initial fields

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var request = require('request');
 var NodeCache = require( "node-cache" );
 
 var myCache = new NodeCache( { stdTTL: 100, checkperiod: 120 } );
@@ -18,10 +19,82 @@ function numberAndSecondaryStat (number, text) {
   return output;
 }
 
-app.get('/makerparty/hosts', function(req, res) {
-  var output = numberAndSecondaryStat(5, 'event hosts');
-  res.json(output);
+function getFromMakerParty (fieldName, callback) {
+  var mpEventsURL = 'http://party.webmaker.org/event-stats';
+
+  request(mpEventsURL, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      // response was successful, so parse the JSON
+      var mpStats = JSON.parse(body);
+      // try and get the requested field from the body
+      if (mpStats[fieldName]) {
+        return callback(mpStats[fieldName]);
+      }
+      return callback('Missing field');
+    } else {
+      callback('Error');
+    }
+  });
+}
+
+function makerPartySingleNumber (res, key, fieldName) {
+  var cached = myCache.get(key);
+  if (cached[key]) {
+    // this was already saved in the cache
+    res.json (cached[key]);
+  } else {
+    // we need to fetch this from makerparty data
+    getFromMakerParty(fieldName, function (result) {
+      var output = numberAndSecondaryStat(result, fieldName);
+      // save this to cache for quicker round trip next time
+      myCache.set (key, output);
+      res.json(output);
+    });
+  }
+}
+
+/**
+ * ROUTES
+ */
+
+// MAKER PARTY HOSTS
+var mp_hosts = '/makerparty/hosts';
+app.get(mp_hosts, function(req, res) {
+  makerPartySingleNumber(res, mp_hosts, 'hosts');
 });
+
+var mp_attendees = '/makerparty/attendees';
+app.get(mp_attendees, function(req, res) {
+  makerPartySingleNumber(res, mp_attendees, 'attendees');
+});
+
+var mp_events = '/makerparty/events';
+app.get(mp_events, function(req, res) {
+  makerPartySingleNumber(res, mp_events, 'events');
+});
+
+var mp_countries = '/makerparty/countries';
+app.get(mp_countries, function(req, res) {
+  makerPartySingleNumber(res, mp_countries, 'countries');
+});
+
+var mp_cities = '/makerparty/cities';
+app.get(mp_cities, function(req, res) {
+  makerPartySingleNumber(res, mp_cities, 'cities');
+});
+
+var mp_mentors = '/makerparty/mentors';
+app.get(mp_mentors, function(req, res) {
+  makerPartySingleNumber(res, mp_mentors, 'mentors');
+});
+
+var mp_coorganizers = '/makerparty/coorganizers';
+app.get(mp_coorganizers, function(req, res) {
+  makerPartySingleNumber(res, mp_coorganizers, 'coorganizers');
+});
+
+
+
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
